38cc6d4c278ded913fdf813e548b463f68ad889e
KevinSizz <kevinskate.kg@gmail.com>
2020-06-19 18:47:30 -0500

    Removing extra code

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
 
 import React,{Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
 import Navigator from './src/Navigator'
 
 export default class App extends Component {
