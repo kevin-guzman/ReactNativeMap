3d4f14853d64d970e5f077fbf9c5e80e336c95d6
KevinSizz <kevinskate.kg@gmail.com>
2020-07-14 19:16:49 -0500

    Added forms hook (useForm)

---
+++ src/custom/hooks/useForm.js
@@ -0,0 +1,31 @@
+import {useState} from 'react'
+
+let useForm = ({initialValues}) => {
+    const [fields, setFields] = useState(initialValues)
+    const addField = (name, value) =>{
+        setFields({...fields, [name]: value})
+    }
+    const removeField = (name) =>{
+        const filtered = fields.filter((x)=> x !== name )
+        setFields(filtered)
+    }
+    return{
+        fields,
+        addField,
+        removeField,
+        getInput: (name) => ({
+            name,
+            onChangeText : (text) =>{
+                setFields({...fields, [name]:text})
+            }
+        }),
+        getPicker: (name) => ({
+            name,
+            onValueChange : (value) =>{
+                setFields({...fields, [name]:value})
+            }
+        }),
+    }
+}
+
+export default useForm
--- src/screens/AddMarker.js
+++ src/screens/AddMarker.js
@@ -4,104 +4,96 @@ import {
     View,
     StatusBar,
 } from 'react-native';
-import { TextInput, Button, Title } from 'react-native-paper';
+import { TextInput, Button, Title, Text } from 'react-native-paper';
 import {Picker} from '@react-native-community/picker';
 import {connect} from 'react-redux'
 import {addMarker, removeMarker} from '../reduxSrc/actions/markers'
+import useForm from '../custom/hooks/useForm'
 
+const initialValues ={
+    MarkerName:'Nombre del marcador',
+    MarkerAdress:'Direccion del marcador',
+    MarkerType: 'Otros'
+}
+const TypeList=["Covid", "Odontologia", "General", "Otros"]
 
-class AddMarker extends Component{
-    constructor(props){
-        super(props)
-        this.TypeList=["Selecciona el tipo de hospital","Covid", "Odontologia", "General", "Otros"]
-        this.state={
-            MarkerName:'',
-            MarkerType:'',
-            selectedItem:'',
-            MarkerAdress:'',
-        }
-    }
-    markerTypeList = () =>{
-        return(
-            this.TypeList.map((x,i)=>{
-                return(
-                    <Picker.Item
-                        label={x}
-                        key={i}
-                        value={x}
-                    />
-                )
-            })
-        )
-    }
-    add = async() =>{
-        const {navigation}=this.props
-        const latitude = navigation.getParam('lat')
-        const longitude = navigation.getParam('lng')
-        const {MarkerName} = this.state;
-        const {MarkerType} = this.state;
-        const {MarkerAdress} = this.state;
-        this.props.addMarker(
-            {
-                latitude: latitude,
-                longitude: longitude,
-                title: MarkerName,
-                address: MarkerAdress,
-                category: MarkerType
-            }
-        )
+const markerTypeList = () =>{
+    return(
+        TypeList.map((x,i)=>{
+            return(
+                <Picker.Item
+                    label={x}
+                    key={i}
+                    value={x}
+                />
+            )
+        })
+    )
+}
+
+const AddMarker = (props) => {
+    const form = useForm({initialValues})
+    const PickerValue = form.fields.MarkerType
+    const {navigation}=props
+    const latitude = navigation.getParam('lat')
+    const longitude = navigation.getParam('lng')
+
+    const Add = () =>{
+        props.addMarker({
+            latitude: latitude,
+            longitude: longitude,
+            title: form.fields.MarkerName,
+            address: form.fields.MarkerAdress,
+            category: form.fields.MarkerType
+        })
         navigation.navigate('MapScreen')
     }
-    render(){
-        const {navigation}=this.props
-        return(
-            <View style={styles.container} >
-                <View>
-                    <Title
-                        style={{alignSelf:"center", marginTop:50}}
+
+    return(
+        <View style={styles.container} >
+            <Title
+                style={styles.title}
+            >
+                ¡Agrega tu marcador!
+            </Title>
+            <TextInput
+                style={styles.textInput}
+                label='Escribe el nombre del marcador'
+                {...form.getInput('MarkerName')}
+            />
+            <TextInput
+                style={styles.textInput}
+                label='Escribe La direccion del marcador'
+                {...form.getInput('MarkerAdress')}
+            />
+            <View style={styles.body} >
+                <Text style={{textAlign:'center'}} >
+                    Selecciona el tipo de hospital
+                </Text>
+                <View style={styles.picker} >
+                    <Picker
+                        {...form.getPicker('MarkerType')}
+                        selectedValue={PickerValue}
+                        
                     >
-                        ¡Agrega tu marcador!
-                    </Title>
-                    <TextInput
-                        style={{marginHorizontal:'5%', marginTop:20}}
-                        label='Escribe el nombre del marcador'
-                        onChangeText={(MarkerName)=> this.setState({MarkerName})} 
-                    />
-                    <TextInput
-                        style={{marginHorizontal:'5%', marginTop:'5%'}}
-                        label='Escribe La direccion del marcador'
-                        onChangeText={(MarkerAdress)=> this.setState({MarkerAdress})} 
-                    />
-                    <View style={styles.picker} >
-                        <Picker
-                            selectedValue= {(this.state && this.state.MarkerType) || 'a'} //{this.state.selectedItem}
-                            onValueChange={(x,i)=>{
-                                this.setState({MarkerType: x, selectedItem: x})
-                                console.log(x)
-                            }}
-                            
-                        >
-                            {this.markerTypeList()}
-                        </Picker>
-                    </View>
-                    
-                    <View style={{marginVertical:'10%', alignSelf:'center', flexDirection:'row',marginHorizontal:'5%'}} >
-                        <Button
-                            onPress={()=>navigation.navigate('MapScreen')}
-                        >
-                            Volver al mapa
-                        </Button> 
-                        <Button
-                            onPress={()=>this.add()}
-                        >
-                            Guardar marcador
-                        </Button>
-                    </View>
-                    
+                        {markerTypeList()}
+                    </Picker>
                 </View>
             </View>
-        )
-    }
+            <View style={styles.footer} >
+                <Button
+                    onPress={()=>navigation.navigate('MapScreen')}
+                >
+                    Volver al mapa
+                </Button> 
+                <Button
+                    onPress={()=>Add()}
+                >
+                    Guardar marcador
+                </Button>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -113,11 +105,25 @@ const styles = StyleSheet.create({
     picker:{
         borderWidth:1,
         borderColor:'gray',
-        marginTop: 15, 
+    },
+    title:{
+        alignSelf:"center", 
+        marginTop:50,
+    },
+    textInput:{
+        marginHorizontal:'5%', 
+        marginTop:20,
+    },
+    body:{
         marginTop: '10%',
-        marginHorizontal:'5%'
+        marginHorizontal:'5%',
+    },
+    footer:{
+        marginVertical:'30%', 
+        alignSelf:'center', 
+        flexDirection:'row',
+        marginHorizontal:'5%',
     }
-
 })
 
 export default connect(
