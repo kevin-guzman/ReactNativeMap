ac5a06a231f727a04269a888e6f9a623686ed00e
KevinSizz <kevinskate.kg@gmail.com>
2020-07-14 19:35:15 -0500

    Map component as a functional component

--- src/reduxSrc/reducers/markers.js
+++ src/reduxSrc/reducers/markers.js
@@ -1,16 +1,9 @@
 export default (state=[],action)=>{
-    const FirstMarker= {
-        latitude: 4.699050, 
-        longitude: -74.050105, 
-        category: 'General', 
-        title:'Tunal'
-    }
-    state.concat([FirstMarker])
     switch(action.type){
         case 'ADD_MARKER':
             return state.concat([action.marker])
         case 'REMOVE_MARKER':
-            return state.filter((event)=> marker.id != action.marker.id )
+            return state.filter((mark)=> mark.id != action.marker.id )
         case 'CLEAR_MARKERS':
             return []
         default:
--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component, useState, useEffect} from 'react'
 import { 
     StyleSheet, 
     View,
@@ -19,94 +19,70 @@ import CovidMarker from '../utils/Img/MapMarkers/CovidMarker.png'
 import GeneralMarker from '../utils/Img/MapMarkers/GeneralMarker.png'
 import OdontologiaMarker from '../utils/Img/MapMarkers/OdontologiaMarker.png'
 
-class App extends Component {
-    constructor (props){
-    super(props);
-    this.state={
-        Nombre:null,
-        Hospitals:[ 
-                    {latitude: 4.699050, longitude: -74.050105, category: 'General', title:'Tunal'},
-                    {latitude: 4.671959, longitude: -74.083579, category: 'Covid', title:'Chricales'},
-                    {latitude: 4.683959, longitude: -74.083579, category: 'Odontologia', title:'Country'},
-                    {latitude: 4.671959, longitude: -74.050105, title:'Kennedy'},
-        ], 
-        Mapa:[],
-        location:null,
-        errorMsg:null,
-        UserLatitude:0.0,
-        UserLongitude:0.0,
-        HospitalCategory:'NoSelected',
-        Aux:0,
-        KeyRefresh:1,
-        };
-    }
-    touchedOpacity = (ref) =>{
-        this.setState({
-            HospitalCategory: ref
-        })
-    }
-    onMapPress = (MarkerCoord) =>{
-        const {navigation}=this.props
-        let m= MarkerCoord.nativeEvent.coordinate
-        this.setState({KeyRefresh: this.state.KeyRefresh +1})
+
+let Mapa = (props) =>{
+    const {navigation}=props
+    const [hospitalCategory, sethospitalCategory]= useState('NoSelected')
+
+    const onMapPress = (MarkerCoord) =>{
+        let m = MarkerCoord.nativeEvent.coordinate
         navigation.navigate('AddMarker',{
             lat: m.latitude,
             lng: m.longitude
         })
     }
-    render(){        
-        return (
-            <View style={styles.container} >
-                <View style={styles.mapContainer}  >
-                    <RenderMap
-                        Hospitals={ this.props.markers } // this.props.markers
-                        HospitalCategory={this.state.HospitalCategory}
-                        onMapPress={this.onMapPress}
-                    />
-                </View>
-                <View style={styles.footer} >
-                    <ButtonMap
-                        touchedOpacity={this.touchedOpacity}
-                        title="Odontologia"
-                        Img={OdontologiaMarker}
-                        bgColor='rgba(71,185,219,0.5)'
-                        brColor='rgba(26,100,122,0.8)'
-                    />
-                    <ButtonMap
-                        touchedOpacity={this.touchedOpacity}
-                        title="Covid"
-                        Img={CovidMarker}
-                        bgColor='rgba(91,225,100,0.5)'
-                        brColor='rgba(44,129,49,0.8)'
-                    />
-                    <ButtonMap
-                        touchedOpacity={this.touchedOpacity}
-                        title="General"
-                        Img={GeneralMarker}
-                        bgColor='rgba(71,185,219,0.5)'
-                        borColor='rgba(26,100,122,0.8)'
-                    />
-                    <View style={{fle:1,marginTop:'1%',  alignItems:'flex-end', marginHorizontal:'0%'}} >
-                        <TouchableOpacity
-                            onPress={(KeyRefresh)=>{ //this.Reading(KeyRefresh)
-                                this.setState({
-                                    HospitalCategory: 'NoSelected'
-                                })
-                                
-                                }
-                            } 
-                        >
-                            <Image 
-                                source={Reload}
-                                style={styles.image}
-                            />            
-                        </TouchableOpacity>
-                    </View>
+    const touchedOpacity = (ref) =>{
+        sethospitalCategory(ref)
+    }
+
+    
+
+    return(
+        <View style={styles.container} >
+            <View style={styles.mapContainer}  >
+                <RenderMap
+                    Hospitals={ props.markers } // this.props.markers
+                    HospitalCategory={hospitalCategory}
+                    onMapPress={onMapPress}
+                />
+            </View>
+            <View style={styles.footer} >
+                <ButtonMap
+                    touchedOpacity={touchedOpacity}
+                    title="Odontologia"
+                    Img={OdontologiaMarker}
+                    bgColor='rgba(71,185,219,0.5)'
+                    brColor='rgba(26,100,122,0.8)'
+                />
+                <ButtonMap
+                    touchedOpacity={touchedOpacity}
+                    title="Covid"
+                    Img={CovidMarker}
+                    bgColor='rgba(91,225,100,0.5)'
+                    brColor='rgba(44,129,49,0.8)'
+                />
+                <ButtonMap
+                    touchedOpacity={touchedOpacity}
+                    title="General"
+                    Img={GeneralMarker}
+                    bgColor='rgba(71,185,219,0.5)'
+                    borColor='rgba(26,100,122,0.8)'
+                />
+                <View style={{fle:1,marginTop:'1%',  alignItems:'flex-end', marginHorizontal:'0%'}} >
+                    <TouchableOpacity
+                        onPress={()=> sethospitalCategory('NoSelected')} 
+                    >
+                        <Image 
+                            source={Reload}
+                            style={styles.image}
+                        />            
+                    </TouchableOpacity>
                 </View>
             </View>
-        );
-    }
+        </View>
+    )
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -139,4 +115,4 @@ export default connect(
         removeMarker
     }
 ) 
-(App);
+(Mapa);
